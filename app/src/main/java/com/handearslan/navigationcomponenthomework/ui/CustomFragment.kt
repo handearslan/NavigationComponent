6d93324d5f156f95e848d5062d29136fa61ffdbc
handearslan <hande_arslan1999@hotmail.com>
2023-09-12 23:11:13 +0300

    Navigation Component Homework is done.

--- app/src/main/java/com/handearslan/navigationcomponenthomework/ui/CustomFragment.kt
+++ app/src/main/java/com/handearslan/navigationcomponenthomework/ui/CustomFragment.kt
@@ -21,9 +21,9 @@ class CustomFragment : Fragment(R.layout.fragment_custom) {
         val personalInfo = args.personelInfo
 
         binding.btnSonuclar.setOnClickListener {
-            val soru1 =binding.tvKacYasindasiniz.text.toString()
-            val soru2 =binding.tvNerelisiniz.text.toString()
-            val soru3 =binding.tvMesleginizNedir.text.toString()
+            val soru1 =binding.etKacYasindasiniz.text.toString()
+            val soru2 =binding.etNerelisiniz.text.toString()
+            val soru3 =binding.etMesleginizNedir.text.toString()
             val info = Info(personalInfo,soru1,soru2,soru3)
 
             val action =
